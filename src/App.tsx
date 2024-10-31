32555a487f476b4009a3943716ebed39118d4d15
Richard Allwood <richard.allwood@circlein.com>
2024-11-01 08:47:11 +1000

    added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 function App() {
-
   return (
-    <h1 className="text-3xl font-bold text-red-500 underline text-center">App</h1>
-  )
+    <h1 className="text-3xl font-bold text-red-500 underline text-center">
+      App
+    </h1>
+  );
 }
 
-export default App
+export default App;
