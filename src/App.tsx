a3cba583c6f145413e59f6843d4e6dbaee3686dd
Richard Allwood <richard.allwood@circlein.com>
2024-11-01 09:40:21 +1000

    added react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
+
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
-    <h1 className="text-3xl font-bold text-red-500 underline text-center">
-      App
-    </h1>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="w-[32rem] flex flex-col gap-4 m-4 border border-black rounded-2xl p-4">
+        <div className="gap-4 columns-2">
+          <div className="text-right">Field One:</div>
+          <input
+            className="border border-black rounded px-2 w-full"
+            defaultValue="test"
+            {...register('example')}
+          />
+        </div>
+
+        <div className="gap-4 columns-2">
+          <div className="text-right">Field Two:</div>
+          <div className="flex flex-col">
+            <input
+              className="border border-black rounded px-2 w-full"
+              placeholder="some text is required here"
+              {...register('exampleRequired', { required: true })}
+            />
+            {errors.exampleRequired && (
+              <div className="text-sm text-red-400">This field is required</div>
+            )}
+          </div>
+        </div>
+
+        <input className="rounded border bg-gray-200" type="submit" />
+      </div>
+    </form>
   );
 }
 
