2f494c571ba6a9b263cfda7f220c093a3099e90c
ricall <richard.m.allwood@gmail.com>
2024-11-03 03:56:29 +1000

    Added other shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Button } from '@/components/ui/button.tsx';
+import { Input } from '@/components/ui/input.tsx';
+import { Label } from '@radix-ui/react-label';
 
 const schema = yup
   .object({
@@ -25,15 +27,15 @@ function App() {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="w-[32rem] flex flex-col gap-4 m-4 border border-black rounded-2xl p-4">
-        <div className="gap-4 columns-2">
-          <div className="text-right">Field One:</div>
-          <input className="border border-black rounded px-2 w-full" defaultValue="test" {...register('example')} />
+        <div className="flex flex-col gap-1">
+          <Label>Field One:</Label>
+          <Input className="border border-black rounded px-2 w-full" defaultValue="test" {...register('example')} />
         </div>
 
-        <div className="gap-4 columns-2">
-          <div className="text-right">Field Two:</div>
+        <div className="flex flex-col gap-1">
+          <Label>Field Two:</Label>
           <div className="flex flex-col">
-            <input
+            <Input
               className="border border-black rounded px-2 w-full"
               placeholder="some text is required here"
               {...register('exampleRequired', { required: true })}
